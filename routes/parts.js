b3203f0896ded6d6e3a409e5c69adb81b3c26d17
Patrick Stäbler <p.staebler@gmail.com>
2016-11-30 19:22:39 +0100

    Changed DB

--- routes/parts.js
+++ routes/parts.js
@@ -4,8 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-//var db = mongojs('mongodb://admin:yppj@ds063546.mlab.com:63546/ibsys', ['teile']);
-var db = mongojs('mongodb://localhost:27017/local', ['teile']);
+var db = mongojs('mongodb://admin:yppj@ds063546.mlab.com:63546/ibsys', ['teile']);
+//var db = mongojs('mongodb://localhost:27017/local', ['teile']);
 
 //Get All Parts
 router.get('/parts', function (req, res, next) {
--- routes/workstations.js
+++ routes/workstations.js
@@ -4,8 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-//var db = mongojs('mongodb://admin:yppj@ds063546.mlab.com:63546/ibsys',['arbeitsplaetze']);
-var db = mongojs('mongodb://localhost:27017/local',['arbeitsplaetze']);
+var db = mongojs('mongodb://admin:yppj@ds063546.mlab.com:63546/ibsys',['arbeitsplaetze']);
+//var db = mongojs('mongodb://localhost:27017/local',['arbeitsplaetze']);
 
 //Get All workstations
 router.get('/workstations', function(req, res, next){
