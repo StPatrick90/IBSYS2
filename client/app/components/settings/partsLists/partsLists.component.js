c54021e207b459ddd60ee587dfd8e4ba920d5077
Patrick Stäbler <p.staebler@gmail.com>
2016-12-08 16:23:49 +0100

    Stückliste Zwischenstand

--- client/app/components/settings/partsLists/partsLists.component.js
+++ client/app/components/settings/partsLists/partsLists.component.js
@@ -13,22 +13,114 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  */
 var core_1 = require('@angular/core');
 var session_service_1 = require('../../../services/session.service');
+var part_1 = require('../../../model/part');
 var part_service_1 = require('../../../services/part.service');
 var PartsListsComponent = (function () {
     function PartsListsComponent(partservice, sessionService) {
         var _this = this;
         this.partservice = partservice;
         this.sessionService = sessionService;
+        this.part = new part_1.Part();
+        this.nodes = 5;
+        this.productOptions = Array();
+        this.getNumber = function (num) {
+            return new Array(num);
+        };
         if (this.sessionService.getParts() != null || this.sessionService.getParts() != undefined) {
             this.parts = this.sessionService.getParts();
+            this.pparts = this.sessionService.getParts().filter(function (item) { return item.typ == "P"; });
+            this.initMultiSelects();
         }
         else {
             this.partservice.getParts()
                 .subscribe(function (parts) {
                 _this.parts = parts;
-            });
+                _this.pparts = parts.filter(function (item) { return item.typ == "P"; });
+            }, function (err) { return console.error(err); }, function () { return _this.initMultiSelects(); });
         }
     }
+    PartsListsComponent.prototype.initMultiSelects = function () {
+        for (var _i = 0, _a = this.pparts; _i < _a.length; _i++) {
+            var pt = _a[_i];
+            this.productOptions.push({ id: pt.nummer, name: pt.bezeichnung.toString() });
+        }
+        this.productSettings = {
+            pullRight: false,
+            enableSearch: false,
+            checkedStyle: 'glyphicon',
+            buttonClasses: 'btn btn-default',
+            selectionLimit: 1,
+            closeOnSelect: true,
+            showCheckAll: false,
+            showUncheckAll: false,
+            dynamicTitleMaxItems: 1,
+            maxHeight: '100px',
+        };
+        this.multiSelectTexts = {
+            checkAll: 'Check all',
+            uncheckAll: 'Uncheck all',
+            checked: 'checked',
+            checkedPlural: 'checked',
+            searchPlaceholder: 'Search...',
+            defaultTitle: 'Select',
+        };
+    };
+    PartsListsComponent.prototype.generatePartsList = function () {
+        if (this.auswahl != undefined && this.auswahl.length == 1) {
+            for (var _i = 0, _a = this.parts; _i < _a.length; _i++) {
+                var pt = _a[_i];
+                if (pt.nummer == this.auswahl[0]) {
+                    this.part = pt;
+                }
+            }
+            if (this.part != null) {
+                this.partsList = {
+                    _id: this.part._id,
+                    bezeichnung: this.part.bezeichnung,
+                    typ: this.part.typ,
+                    nummer: this.part.nummer,
+                    anzahl: 1,
+                    bestandteile: []
+                };
+                for (var _b = 0, _c = this.part.bestandteile; _b < _c.length; _b++) {
+                    var best = _c[_b];
+                    for (var _d = 0, _e = this.parts; _d < _e.length; _d++) {
+                        var pt = _e[_d];
+                        if (best._id == pt._id) {
+                            this.partsList.bestandteile.push({
+                                _id: pt._id,
+                                bezeichnung: pt.bezeichnung,
+                                typ: pt.typ,
+                                nummer: pt.nummer,
+                                anzahl: best.anzahl,
+                                bestandteile: this.getBestandteile(pt)
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    };
+    PartsListsComponent.prototype.getBestandteile = function (part) {
+        var bestandteile = Array();
+        for (var _i = 0, _a = part.bestandteile; _i < _a.length; _i++) {
+            var ptOut = _a[_i];
+            for (var _b = 0, _c = this.parts; _b < _c.length; _b++) {
+                var ptIn = _c[_b];
+                if (ptOut._id == ptIn._id) {
+                    bestandteile.push({
+                        _id: ptIn._id,
+                        bezeichnung: ptIn.bezeichnung,
+                        typ: ptIn.typ,
+                        nummer: ptIn.nummer,
+                        anzahl: ptOut.anzahl,
+                        bestandteile: this.getBestandteile(ptIn)
+                    });
+                }
+            }
+        }
+        return bestandteile;
+    };
     PartsListsComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
--- client/app/components/settings/partsLists/partsLists.component.ts
+++ client/app/components/settings/partsLists/partsLists.component.ts
@@ -3,10 +3,14 @@
  */
 import {Component} from '@angular/core';
 import {SessionService} from '../../../services/session.service';
-import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import {ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
 import {Part} from '../../../model/part';
 import {PartService} from '../../../services/part.service';
-import {SessionService} from '../../../services/session.service';
+import {
+    IMultiSelectOption,
+    IMultiSelectSettings,
+    IMultiSelectTexts
+} from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
 
 @Component({
     moduleId: module.id,
@@ -15,18 +19,116 @@ import {SessionService} from '../../../services/session.service';
 })
 export class PartsListsComponent {
     parts: Part[];
+    pparts: Part[];
+    part: Part = new Part();
+    auswahl: any;
+
+    partsList: any;
+    nodes: number = 5;
+
+    private productOptions: IMultiSelectOption[] = Array<IMultiSelectOption>();
+    private productSettings: IMultiSelectSettings;
+    private multiSelectTexts: IMultiSelectTexts;
 
     constructor(private partservice: PartService, private sessionService: SessionService) {
         if (this.sessionService.getParts() != null || this.sessionService.getParts() != undefined) {
             this.parts = this.sessionService.getParts();
+            this.pparts = this.sessionService.getParts().filter(item => item.typ == "P");
+            this.initMultiSelects();
         }
         else {
             this.partservice.getParts()
                 .subscribe(parts => {
-                        this.parts = parts
-                    });
+                        this.parts = parts;
+                        this.pparts = parts.filter(item => item.typ == "P");
+                    },
+                    err => console.error(err),
+                    () => this.initMultiSelects());
         }
     }
 
+    initMultiSelects() {
+        for (let pt of this.pparts) {
+            this.productOptions.push({id: pt.nummer, name: pt.bezeichnung.toString()});
+        }
 
+        this.productSettings = {
+            pullRight: false,
+            enableSearch: false,
+            checkedStyle: 'glyphicon',
+            buttonClasses: 'btn btn-default',
+            selectionLimit: 1,
+            closeOnSelect: true,
+            showCheckAll: false,
+            showUncheckAll: false,
+            dynamicTitleMaxItems: 1,
+            maxHeight: '100px',
+        };
+
+        this.multiSelectTexts = {
+            checkAll: 'Check all',
+            uncheckAll: 'Uncheck all',
+            checked: 'checked',
+            checkedPlural: 'checked',
+            searchPlaceholder: 'Search...',
+            defaultTitle: 'Select',
+        };
+    }
+
+    generatePartsList(){
+        if(this.auswahl != undefined && this.auswahl.length == 1) {
+            for (let pt of this.parts) {
+                if (pt.nummer == this.auswahl[0]) {
+                    this.part = pt;
+                }
+            }
+            if(this.part != null) {
+                this.partsList = {
+                    _id: this.part._id,
+                    bezeichnung: this.part.bezeichnung,
+                    typ: this.part.typ,
+                    nummer: this.part.nummer,
+                    anzahl: 1,
+                    bestandteile: []
+                };
+                for (let best of this.part.bestandteile) {
+                    for (let pt of this.parts) {
+                        if (best._id == pt._id) {
+                            this.partsList.bestandteile.push({
+                                _id: pt._id,
+                                bezeichnung: pt.bezeichnung,
+                                typ: pt.typ,
+                                nummer: pt.nummer,
+                                anzahl: best.anzahl,
+                                bestandteile: this.getBestandteile(pt)
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    getBestandteile(part) {
+        var bestandteile = Array<any>();
+        for (let ptOut of part.bestandteile) {
+            for (let ptIn of this.parts) {
+                if (ptOut._id == ptIn._id) {
+                    bestandteile.push({
+                        _id: ptIn._id,
+                        bezeichnung: ptIn.bezeichnung,
+                        typ: ptIn.typ,
+                        nummer: ptIn.nummer,
+                        anzahl: ptOut.anzahl,
+                        bestandteile: this.getBestandteile(ptIn)
+                    })
+                }
+            }
+        }
+        return bestandteile;
+    }
+
+    getNumber = function(num) {
+        return new Array(num);
+    }
 }
